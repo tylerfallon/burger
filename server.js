0a9a3c29835cc7160cdd028bc8d3322a89b5fda8
Tyler Fallon <tylerfallon@Tylers-MacBook-Pro.local>
2016-08-07 11:51:52 -0400

    server.js file

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var app = express();
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+
+app.use(methodOverride('_method'));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
