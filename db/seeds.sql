e4cc79f0bff91ecf50ef723e3bb667d75a818406
Tyler Fallon <tylerfallon@Tylers-MacBook-Pro.local>
2016-08-07 11:32:53 -0400

    added burgers to the database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDouble', false);
