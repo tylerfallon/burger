91e5d4e49090ee48b4a514c5408984deb4ca2b01
Tyler Fallon <tylerfallon@Tylers-MacBook-Pro.local>
2016-08-07 11:30:58 -0400

    wrote sql

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (id int NOT NULL AUTO_INCREMENT,
+   burger_name VARCHAR(40) NOT NULL,
+   devoured BOOLEAN DEFAULT 0,
+   createdDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   PRIMARY KEY(id)
+ );
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Burger', false);
